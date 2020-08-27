79c44701a6a51937d890f4efa78dfc442cae550b
SohailSattar <69412878+SohailSattar-evetech@users.noreply.github.com>
2020-08-27 18:00:02 +0400

    Added files

---
+++ precache-manifest.8c0873676738d83dcf13dce91accf2fd.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be99c9bd8557800c1596b59fb35317a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ea26e32ebb2a0626e1c",
+    "url": "/static/css/2.0f26c778.chunk.css"
+  },
+  {
+    "revision": "a4759a1160ad983d5600",
+    "url": "/static/js/10.c47e25f6.chunk.js"
+  },
+  {
+    "revision": "e29537b1ff0e471b563d",
+    "url": "/static/js/11.55a346b3.chunk.js"
+  },
+  {
+    "revision": "76f9708ebc60264dc237",
+    "url": "/static/js/12.664f77a9.chunk.js"
+  },
+  {
+    "revision": "19cb31c44bcf1cb88be7",
+    "url": "/static/js/13.95e4b07a.chunk.js"
+  },
+  {
+    "revision": "de3fa7dc5a3b7343cfff",
+    "url": "/static/js/14.326ce989.chunk.js"
+  },
+  {
+    "revision": "68a6cd40ab677e89db94",
+    "url": "/static/js/15.b5a0af68.chunk.js"
+  },
+  {
+    "revision": "2f336a2e37b80716566d",
+    "url": "/static/js/16.36bd73b9.chunk.js"
+  },
+  {
+    "revision": "97d34aac401ccfa59e41",
+    "url": "/static/js/17.3fc1b7be.chunk.js"
+  },
+  {
+    "revision": "c3caab8c8d7976c89516",
+    "url": "/static/js/18.0fac419e.chunk.js"
+  },
+  {
+    "revision": "7f68faaeff8231a1b0bd",
+    "url": "/static/js/19.06095b9e.chunk.js"
+  },
+  {
+    "revision": "8ea26e32ebb2a0626e1c",
+    "url": "/static/js/2.1acf82f9.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.1acf82f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe56372e92f214e96e4",
+    "url": "/static/js/20.db293bd3.chunk.js"
+  },
+  {
+    "revision": "f824696bebdfa8e2526c",
+    "url": "/static/js/21.26ac9a11.chunk.js"
+  },
+  {
+    "revision": "2091d7bdb415b7aa9d70",
+    "url": "/static/js/22.fe233038.chunk.js"
+  },
+  {
+    "revision": "196d40bf230d1660ba76",
+    "url": "/static/js/23.d0ae9089.chunk.js"
+  },
+  {
+    "revision": "01b0dcf31142f4559757",
+    "url": "/static/js/24.5b4b1f0c.chunk.js"
+  },
+  {
+    "revision": "3ed2516d6c2570061f2e",
+    "url": "/static/js/25.1b36a379.chunk.js"
+  },
+  {
+    "revision": "e5c66d707d5d7b3752d9",
+    "url": "/static/js/26.efad14b9.chunk.js"
+  },
+  {
+    "revision": "9ffaae70fe2af812ae3d",
+    "url": "/static/js/27.afb97e72.chunk.js"
+  },
+  {
+    "revision": "700982394cb2433fa11b",
+    "url": "/static/js/28.9cd161fc.chunk.js"
+  },
+  {
+    "revision": "3d46694ec994700059d2",
+    "url": "/static/js/3.95c38c2b.chunk.js"
+  },
+  {
+    "revision": "380736282cde54e106b7",
+    "url": "/static/js/4.daaa6abb.chunk.js"
+  },
+  {
+    "revision": "7611699f389204900812",
+    "url": "/static/js/5.f19e52db.chunk.js"
+  },
+  {
+    "revision": "b295f68100a251b3de5e",
+    "url": "/static/js/6.5e5441e2.chunk.js"
+  },
+  {
+    "revision": "c8b46f70a6eab366c96d",
+    "url": "/static/js/7.3dae870b.chunk.js"
+  },
+  {
+    "revision": "0740cb298177c73a2e13",
+    "url": "/static/js/8.efb089c2.chunk.js"
+  },
+  {
+    "revision": "82cc6dcee08d8a77af69",
+    "url": "/static/js/9.d8942ef5.chunk.js"
+  },
+  {
+    "revision": "ae9ac5636816d74f09f6",
+    "url": "/static/js/main.89b88756.chunk.js"
+  },
+  {
+    "revision": "146ae9c8f52fef48bef8",
+    "url": "/static/js/runtime-main.9e8e7bd0.js"
+  },
+  {
+    "revision": "22e5045a1a99bf73ac3e967913efba04",
+    "url": "/static/media/Effra-Bold.22e5045a.woff"
+  },
+  {
+    "revision": "493f843f74fcac1a22333bc629e8ae55",
+    "url": "/static/media/Effra-Bold.493f843f.eot"
+  },
+  {
+    "revision": "a0a289aadce8668cad18ad6acae7d0fa",
+    "url": "/static/media/Effra-Bold.a0a289aa.woff2"
+  },
+  {
+    "revision": "ba2d1272827c166ad5905d299caa1096",
+    "url": "/static/media/Effra-Bold.ba2d1272.ttf"
+  },
+  {
+    "revision": "2556d7a29ab01dc80b96eb824a049df6",
+    "url": "/static/media/Effra-Regular.2556d7a2.ttf"
+  },
+  {
+    "revision": "431f98002c1ba7a6ef4dad437a207d5c",
+    "url": "/static/media/Effra-Regular.431f9800.woff2"
+  },
+  {
+    "revision": "6ce5607d5ccb27b7b594c8a39797b787",
+    "url": "/static/media/Effra-Regular.6ce5607d.woff"
+  },
+  {
+    "revision": "adfad17cf786f8101bb85cdcbe436536",
+    "url": "/static/media/Effra-Regular.adfad17c.eot"
+  }
+]);
